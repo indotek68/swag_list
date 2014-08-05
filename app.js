23eb2c9d4e41af60554e437ba8382aa0718fb16a
indotek68 <angga.w.putra@gmail.com>
2014-08-05 10:23:57 -0700

    added cdn bootstrap, added maps, changed api to bandsintown

--- app.js
+++ app.js
@@ -10,18 +10,38 @@ var app = express();
 app.use(bodyParser.urlencoded());
 app.set('view engine', 'ejs');
 
-app.get('/',function(req, res){
-	var zip = req.query.zip;
-	var radius = req.query.date;
-	console.log(req.query.searchTerm)
-	var url = "http://api.jambase.com/events?zipCode=95128&radius=50&startDate=2014-08-04&page=0&api_key=dwa8856tnfz4v2d55nye56xg"
-	console.log(url)
+app.get('/home', function(req, res){
+	res.render('home')
+})
+
+app.get('/events', function(req, res){
+	res.render('events')
+})
+
+app.get('/signup', function(req, res){
+	res.render('signup')
+})
+
+app.get('/login', function(req, res){
+	res.render('login')
+})
+
+app.get('/search', function(req, res){
+	var area = req.query.area;
+	var radius = req.query.radius;
+	var date = req.query.date;
+
+	var url = "http://api.bandsintown.com/events/search?location="+ area +"&radius="+radius+"&date="+date+"&format=json&app_id=SWAG_LIST";
+
+	console.log("Url" + url)
+	console.log(date)
 
 	request(url, function(error, response, body){
 		if(!error){
 			var body = JSON.parse(body);
-			console.log(url)
-			res.render('results')
+			// var artist = body.Events.artist;
+			console.log(body)
+			res.render('results', {eventsList: body, date: date})
 		}
 	})
 })
